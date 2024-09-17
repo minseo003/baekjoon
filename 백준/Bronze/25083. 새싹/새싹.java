519de55f22a9af09764780fc407344b32f84b595
minseo003 <159864951+minseo003@users.noreply.github.com>
2024-09-17 14:44:50 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+class Main {
+
+    public void solution() {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.solution();
+    }
+}
