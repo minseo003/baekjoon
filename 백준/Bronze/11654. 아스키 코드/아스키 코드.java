20a61daa7c6d34c0798836e560a26edf8db9aa44
minseo003 <159864951+minseo003@users.noreply.github.com>
2024-09-23 11:31:15 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner kb = new Scanner(System.in);
+
+        Byte num = (byte) kb.next().charAt(0);
+        System.out.println(num);
+
+    }
+
+}
