83fb93081d8527e9ebe1a89ce1bc38055a499925
minseo003 <159864951+minseo003@users.noreply.github.com>
2024-10-01 16:38:19 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 164 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Main {
+
+    public int solution(String str) {
+        String[] alpha = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+
+        if(str.length() == 0) return 0;
+
+        for (int i = 0; i < alpha.length; i++) {
+            if(str.contains(alpha[i])) str = str.replace(alpha[i], "0");
+        }
+        return str.length();
+    }
+
+    public static void main(String[] agrs) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.print(T.solution(str));
+    }
+}
