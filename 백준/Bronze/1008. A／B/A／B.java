0ce90066a9f4c2ca66e4565f7c42e278737c8a85
minseo003 <159864951+minseo003@users.noreply.github.com>
2024-10-02 22:05:25 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        double answer = 0.0;
+
+        int a = kb.nextInt();
+        int b = kb.nextInt();
+
+        answer =(double) a/b;
+        System.out.print(answer);
+    }
+}
