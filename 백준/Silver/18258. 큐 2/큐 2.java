99658447d25d8eabadae55b59747b5c1abf43dd4
minseo003 <159864951+minseo003@users.noreply.github.com>
2024-10-17 12:29:30 +0900

    [Silver IV] Title: 큐 2, Time: 1196 ms, Memory: 338636 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        Main T = new Main();
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] queue = new int[n*2];
+        int front = 0;
+        int rear = -1;
+
+        for(int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+
+            if(s[0].equals("push")) queue[++rear] = Integer.parseInt(s[1]);
+            else if(s[0].equals("pop")) {
+                if(rear - front + 1 == 0) bw.write(-1 + "\n");
+                else {
+                    bw.write(queue[front] + "\n");
+                    queue[front++] = 0;
+                }
+            }
+            else if(s[0].equals("size")) bw.write(rear-front+1 + "\n");
+            else if(s[0].equals("empty")) {
+                if(rear-front+1 == 0) bw.write(1 + "\n");
+                else bw.write(0 + "\n");
+            }
+            else if(s[0].equals("front")) {
+                if(rear-front+1 == 0) bw.write(-1 + "\n");
+                else bw.write(queue[front] + "\n");
+            }
+            else {
+                if(rear-front+1 == 0) bw.write(-1 + "\n");
+                else bw.write(queue[rear] + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
