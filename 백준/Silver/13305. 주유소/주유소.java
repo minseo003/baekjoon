121d3ad849f9958e5f0caece758177fd38ca88e0
minseo003 <159864951+minseo003@users.noreply.github.com>
2024-11-29 11:54:55 +0900

    [Silver III] Title: 주유소, Time: 352 ms, Memory: 31372 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    static int N;
+    static int[] city;
+    static int[] dis;
+    static int answer = 0;
+
+    static int solution() {
+        long minCost = city[0];
+
+        for(int i = 0 ; i < N-1; i++) {
+            if (city[i] < minCost) {
+                minCost = city[i];
+            }
+            answer += (minCost * dis[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        city = new int[N];
+        dis = new int[N-1];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for (int i = 0; i < dis.length; i++) {
+            dis[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine()," ");
+        for (int i = 0; i < city.length; i++) {
+            city[i] = Integer.parseInt(st.nextToken());
+        }
+        bw.write(solution() + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
