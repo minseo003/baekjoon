849bf7cf2352d1a45dd0db8cc2a954eb73296db2
minseo003 <159864951+minseo003@users.noreply.github.com>
2024-12-12 13:34:45 +0900

    [Silver III] Title: 01타일, Time: 308 ms, Memory: 72072 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+    static int[] dp = new int[1000001];
+    static int answer;
+
+    static int solution(int x) {
+        if (dp[x] == -1) {
+            dp[x] =  (solution(x-1) + solution(x-2)) % 15746;
+        }
+        return dp[x];
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = -1;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        bw.write(solution(N) + "\n");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+}
