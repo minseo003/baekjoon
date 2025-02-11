864287307ad291d6c24bfff4b3c93d928064c193
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-02-11 12:42:35 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 115.70 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -5,20 +5,24 @@ class Solution {
         Set<Integer> sumSet = new HashSet<>();
         int n = elements.length;
 
-        // 부분 수열 길이를 1부터 n까지 반복
+        // 모든 부분 수열 길이 탐색
         for (int length = 1; length <= n; length++) {
-            // 모든 시작점에 대해 탐색
-            for (int start = 0; start < n; start++) {
-                int sum = 0;
-                // 현재 부분 수열의 길이만큼 합 구하기
-                for (int i = 0; i < length; i++) {
-                    int index = (start + i) % n;  //  원형 수열 처리
-                    sum += elements[index];
-                }
-                sumSet.add(sum);  // 중복 제거된 합 저장
+            int sum = 0;
+
+            // 첫 번째 부분 수열의 합 구하기
+            for (int i = 0; i < length; i++) {
+                sum += elements[i];
+            }
+            sumSet.add(sum);
+
+            // 슬라이딩 윈도우 방식으로 부분 수열 이동하며 합 갱신
+            for (int start = 1; start < n; start++) {
+                sum -= elements[start - 1]; // 윈도우 왼쪽 원소 제거
+                sum += elements[(start + length - 1) % n]; // 오른쪽 원소 추가 (원형 처리)
+                sumSet.add(sum);
             }
         }
 
-        return sumSet.size();  // 가능한 합 개수 반환
+        return sumSet.size();
     }
 }
