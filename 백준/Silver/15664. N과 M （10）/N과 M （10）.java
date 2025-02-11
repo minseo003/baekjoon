b2077a4c6acd57d8ecad0d295c6f232f4d34777d
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-02-11 11:45:32 +0900

    [Silver II] Title: N과 M (10), Time: 116 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.java
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -3,54 +3,51 @@ import java.io.*;
 
 class Main {
 
+    static ArrayList<Integer> answer = new ArrayList<>();
     static int[] arr;
     static boolean[] ch;
-    static ArrayList<Integer> tmp = new ArrayList<>();
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         arr = new int[n];
         ch = new boolean[n];
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < n; i++) {
+
+        st = new StringTokenizer(br.readLine()," ");
+
+        for(int i = 0 ; i < n ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
         Arrays.sort(arr);
         dfs(0,0,m);
-        bw.write(sb.toString() + " \n");
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.print(sb.toString());
     }
+
 
     static void dfs(int start, int depth, int m) {
 
-        Set<Integer> hashSet = new HashSet<>();  // 같은 depth 내 중복 방지
+        Set<Integer> hash = new HashSet<>();
 
         if (depth == m) {
-            for (int x : tmp) {
+            for (int x : answer) {
                 sb.append(x + " ");
             }
             sb.append("\n");
-            return;
         }
 
-        for (int i = start; i < arr.length ; i++) {
-
-            if (!ch[i] && !hashSet.contains(arr[i])) {
-
-                ch[i]= true;
-                hashSet.add(arr[i]);
-                tmp.add(arr[i]);
+        for (int i = start; i < arr.length; i++) {
+            if (!ch[i] && !hash.contains(arr[i])) {
+                ch[i] = true;
+                hash.add(arr[i]);
+                answer.add(arr[i]);
                 dfs(i + 1, depth + 1, m);
                 ch[i] = false;
-                tmp.remove(tmp.size() - 1);
+                answer.remove(answer.size() - 1);
             }
         }
     }
