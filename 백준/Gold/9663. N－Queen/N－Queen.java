a141b2268be4825a1d9011ac4f76564f8a25bc72
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-02-16 16:24:10 +0900

    [Gold IV] Title: N-Queen, Time: 6228 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,43 +1,50 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-    static int N;
-    static int[] arr;
-    static int count = 0;
 
-    public static void dfs(int depth) {
-        if(depth == N) {
+class Main {
+    static int n;
+    static int count = 0;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        dfs(0);
+        bw.write(count + " \n");
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+
+
+    static void dfs(int depth) {
+
+        if (depth == n) {
             count++;
             return;
         }
-        for (int i = 0; i < N; i++) {
+
+        for (int i = 0; i < n; i++) {
             arr[depth] = i;
             if (possible(depth)) {
-                dfs(depth+1);
+                dfs(depth + 1);
             }
         }
     }
 
-    public static boolean possible(int col) {
+    static boolean possible(int col) {
         for (int i = 0; i < col; i++) {
             if (arr[i] == arr[col]) {
                 return false;
-            }
-            else if(Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+            } else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
                 return false;
             }
         }
         return true;
     }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        br.close();
-        arr = new int[N];
-        dfs(0);
-        System.out.println(count);
-    }
-
 }
