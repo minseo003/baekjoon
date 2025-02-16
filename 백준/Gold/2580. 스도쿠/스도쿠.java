75bba0b60a14146db2c4cc55a18ea930b9a2c79c
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-02-16 15:56:08 +0900

    [Gold IV] Title: 스도쿠, Time: 452 ms, Memory: 27256 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -4,50 +4,62 @@ import java.io.*;
 
 class Main {
 
-   static int[][] board = new int[9][9];
-   static StringBuffer sb  = new StringBuffer();
+    static int[][] arr = new int[9][9];
 
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-   static void dfs(int row, int col) {
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-       if (col == 9) {
-           dfs(row + 1,0);
-           return;
-       }
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dfs(0,0);
+        br.close();
+    }
 
-       if (row == 9) {
-           for (int i = 0; i < 9; i++) {
-               for (int j = 0; j < 9; j++) {
-                   System.out.print(board[i][j] + " ");
-               }
-               System.out.println();
-           }
-           System.exit(0);
-       }
+    static void dfs(int row, int col) {
 
-       if (board[row][col] == 0) {
-           for (int i = 1; i <= 9; i++) {
-               if (possibility(row, col, i)) {
-                   board[row][col] = i;
-                   dfs(row,col+1);
-               }
-           }
-           board[row][col] = 0;
-           return;
-       }
-       dfs(row,col+1);
-   }
+        if (col == 9) {
+            dfs(row + 1,0);
+            return;
+        }
+
+        if (row == 9) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
+            }
+            System.exit(0);
+        }
+
+        if (arr[row][col] == 0) {
+            for (int i = 1; i <= 9; i++) {
+                if (possibility(row, col, i)) {
+                    arr[row][col] = i;
+                    dfs(row,col+1);
+                }
+            }
+            arr[row][col] = 0;
+            return;
+        }
+        dfs(row, col + 1);
+    }
 
     public static boolean possibility(int row, int col, int value) {
 
         for (int i = 0; i < 9; i++) {
-            if (board[row][i] == value) {
+            if(arr[row][i] == value) {
                 return false;
             }
         }
 
         for (int i = 0; i < 9; i++) {
-            if (board[i][col] == value) {
+            if (arr[i][col] == value) {
                 return false;
             }
         }
@@ -57,24 +69,11 @@ class Main {
 
         for (int i = set_row; i < set_row + 3; i++) {
             for (int j = set_col; j < set_col + 3; j++) {
-                if (board[i][j] == value) {
+                if (arr[i][j] == value) {
                     return false;
                 }
             }
         }
         return true;
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for(int i = 0 ; i < 9 ; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            for(int j = 0 ; j < 9 ; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        br.close();
-        dfs(0,0);
     }
 }
