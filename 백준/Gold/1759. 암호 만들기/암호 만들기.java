598d09f64198d6bf963aeba8753a8a2bf0280daf
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-03-03 17:27:52 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 15868 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -4,65 +4,54 @@ import java.io.*;
 
 class Main {
 
-    static char[] arr;
-    static boolean[] ch;
-    static Set<Character> hash = new HashSet<>();
     static StringBuilder sb = new StringBuilder();
+    static Character[] arr;
+    static boolean[] ch;
+    static int l;
+    static HashSet<Character> set = new HashSet<>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        hash.add('a');
-        hash.add('e');
-        hash.add('o');
-        hash.add('i');
-        hash.add('u');
-
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int l = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        l = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        arr = new char[c];
+        arr = new Character[c];
         ch = new boolean[c];
-
         st = new StringTokenizer(br.readLine()," ");
-
-        for(int i = 0; i < c ; i++) {
-            arr[i]  = st.nextToken().charAt(0);
+        for (int i = 0; i < c; i++) {
+            arr[i] = st.nextToken().charAt(0);
         }
         Arrays.sort(arr);
-        dfs("",0,0,0,l);
+        dfs(0,new StringBuilder(),0,0);
         bw.write(sb.toString());
         br.close();
         bw.flush();
         bw.close();
-
     }
 
+    static void dfs(int index, StringBuilder str, int xnum, int ynum) {
 
-    static void dfs(String str, int xCnt, int yCnt, int start, int l) {
-
-        if (str.length() == l) {
-            if (xCnt >= 2 && yCnt >= 1) {
-                sb.append(str + "\n");
-            }
+        if (str.length() == l && xnum >= 1 && ynum >= 2) {
+            sb.append(str).append("\n");
             return;
         }
 
-        for(int i = start ; i < arr.length ; i++) {
-
-            if (!ch[i]) {
-
-                ch[i] = true;
-                if (hash.contains(arr[i])) {
-                    yCnt++;
-                }else xCnt++;
-                dfs(str + arr[i], xCnt, yCnt,i+1 , l);
-                ch[i] = false;
-                if (hash.contains(arr[i])) {
-                    yCnt--;
-                } else xCnt--;
+        for (int i = index; i < arr.length; i++) {
+            str.append(arr[i]);
+            if (set.contains(arr[i])) {
+                dfs(i + 1, str, xnum + 1, ynum);
+            } else {
+                dfs(i + 1, str, xnum, ynum + 1);
             }
+            str.deleteCharAt(str.length() - 1);
         }
     }
 }
