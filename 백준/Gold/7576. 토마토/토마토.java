424ab158fd1bf1a34d75c50f41ffe38e052a633c
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-04-12 18:26:19 +0900

    [Gold V] Title: 토마토, Time: 596 ms, Memory: 120860 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,79 +1,79 @@
 import java.util.*;
 import java.io.*;
 
+class Tomato {
+    int x;
+    int y;
+
+    public Tomato(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 class Main {
 
-    static class Point {
-        int x;
-        int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    static int M;  //열
-    static int N;  //행
+    static int m,n;
+    static int[] dx = {1,0,-1,0};
+    static int[] dy = {0,1,0,-1};
     static int[][] board;
-    static int answer = 0;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
-    static Queue<Point> queue = new LinkedList<>();
+    static Queue<Tomato> queue = new LinkedList<>();
     static boolean flag = false;
 
-    static void bfs() {
 
-        while (!queue.isEmpty()) {
-            Point p =queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M && board[nx][ny] == 0) {
-                    board[nx][ny] = board[p.x][p.y] + 1;
-                    queue.offer(new Point(nx,ny));
-                }
-            }
-        }
-
-
-    }
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        board = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine()," ");
-            for (int j = 0; j < M; j++) {
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
-                if(board[i][j] == 1){
-                    queue.offer(new Point(i,j));
-                }
+                if(board[i][j] == 1) queue.offer(new Tomato(i,j));
+                if(board[i][j] == 0) flag = true;
             }
         }
-        br.close();
         bfs();
-        for (int i = 0; i < N; i++) {
-            if (flag) {
-                answer = -1;
-                break;
-            }
-            for(int j = 0 ; j < M ; j++) {
-                if (board[i][j] == 0) {
-                    flag = true;
+
+        int answer = 0;
+        boolean zeroLeft = false;
+        for (int i = 0; i < n; i++) {
+            for(int j = 0 ; j < m ; j++){
+                if(board[i][j] == 0) {
+                    answer = -1;
+                    zeroLeft = true;
+                    break;
                 }
-                answer = Math.max(answer,board[i][j]-1);
+                answer = Math.max(board[i][j]-1, answer);
             }
+            if(zeroLeft) break;
         }
-        bw.write(answer + " ");
+        if(!flag) answer = 0;
+
+
+        bw.write(answer + "\n");
+        br.close();
         bw.flush();
         bw.close();
+    }
 
+    static void bfs() {
+
+        while(!queue.isEmpty()) {
+             Tomato t = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = t.x + dx[i];
+                int ny = t.y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
+                    board[nx][ny] = board[t.x][t.y] + 1;
+                    queue.offer(new Tomato(nx,ny));
+                }
+            }
+        }
     }
 }
