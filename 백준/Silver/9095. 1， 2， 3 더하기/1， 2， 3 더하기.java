c99344417aca6c92c842d90dc1e8f606dc98718f
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-04-16 16:01:22 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 112 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -3,38 +3,28 @@ import java.io.*;
 
 class Main {
 
-    public List<Integer> solution(int[] arr) {
-        int[] dp = new int[11];
-        List<Integer> alist = new ArrayList<>();
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        int[] dp = new int[12];
+
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
 
-        for(int i = 4;i<dp.length;i++) {
+        for(int i = 4; i <= 11; i++) {
             dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
-        for(int i=0;i<arr.length;i++) {
-            alist.add(dp[arr[i]]);
-        }
-        return alist;
-    }
 
-    public static void main(String[] args) throws Exception {
-        Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i < t ; i++) {
+            int x = Integer.parseInt(br.readLine());
+            bw.write(dp[x] + "\n");
         }
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        for(int x : T.solution(arr)) {
-            bw.write(x + "\n");
-        }
+        br.close();
         bw.flush();
         bw.close();
+        
     }
-
 }
