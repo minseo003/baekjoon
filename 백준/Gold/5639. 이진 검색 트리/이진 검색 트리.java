b4098fa940a2de24f9e9385502a32872860a2a53
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-05-12 14:00:44 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 596 ms, Memory: 19560 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    static class Node {
+        int val;
+        Node left, right;
+
+        Node(int val) {
+            this.val = val;
+        }
+
+        Node(int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        void insert(int n) {
+            if(n < this.val) {
+                if(this.left == null) this.left = new Node(n);
+                else this.left.insert(n);
+            } else {
+                if(this.right == null) this.right = new Node(n);
+                else this.right.insert(n);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Node root = new Node(Integer.parseInt(br.readLine()));
+
+        while(true) {
+            String str = br.readLine();
+
+            if(str == null || str.equals("")) break;
+
+            int val = Integer.parseInt(str);
+            root.insert(val);
+        }
+        postOrder(root);
+    }
+
+    static void postOrder(Node curr) {
+        if(curr == null) return;
+
+        postOrder(curr.left);
+        postOrder(curr.right);
+        System.out.println(curr.val);
+    }
+}
