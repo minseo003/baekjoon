ae297cf8a692a0eb31630b1b0f8764870d8cd9ba
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-05-15 13:28:33 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -3,29 +3,38 @@ import java.io.*;
 
 class Main {
 
-    public String solution(String s) {
-        Stack<Character> stack = new Stack<>();
-        String answer = "YES";
-        for(char x : s.toCharArray()) {
-            if(x=='(') stack.push(x);
-            else if(stack.size() != 0) stack.pop();
-            else return "NO";
-        }
-        if(stack.size() != 0) return "NO";
-        return answer;
-    }
-
-    public static void main(String[] args) throws Exception{
-        Main T = new Main();
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        for(int i=0;i<n;i++) {
-            String s = br.readLine();
-            bw.write(T.solution(s) + "\n");
+        for(int i = 0 ; i < n ; i++) {
+            boolean flag = false;
+            String str = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            for(int j = 0 ; j < str.length(); j++) {
+                if(str.charAt(j) == '(') {
+                    stack.push(str.charAt(j));
+                }else {
+                    if(stack.isEmpty()) {
+                        flag = true;
+                        break;
+                    }else {
+                        stack.pop();
+                    }
+                }
+            }
+            if(!flag && stack.isEmpty()) {
+                sb.append("YES").append('\n');
+            } else {
+                sb.append("NO").append('\n');
+            }
         }
+        bw.write(sb.toString());
+        br.close();
         bw.flush();
         bw.close();
+
     }
 }
