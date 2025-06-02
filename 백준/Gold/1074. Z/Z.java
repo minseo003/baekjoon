a42b5789d09ae63e308311cf30c11bd2384fb2ca
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-06-02 14:30:45 +0900

    [Gold V] Title: Z, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -11,26 +11,26 @@ class Main {
         int N = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        int size = (int) Math.pow(2, N);
+        int size = (int) Math.pow(2, N);  // 2의 N제곱을 구하는 코드
 
-        find(size,r,c);
+        find(size,r,c); //재귀 시작
         System.out.println(count);
 
     }
 
     static void find(int size, int r, int c) {
-        if(size == 1) return;
+        if(size == 1) return;  //배열크기가 1이면 더이상 나눌 수 없으니 탐색종료
 
-        if(r < size/2 && c < size/2) {
+        if(r < size/2 && c < size/2) { // 1사분면
             find(size/2,r,c);
-        }else if(r < size/2 && c >= size/2) {
-            count += size * size/4;
+        }else if(r < size/2 && c >= size/2) { // 2사분면
+            count += size * size / 4;  // 전체크기의 1/4만큼 더하고 시작
             find(size/2,r,c-size/2);
-        } else if(r >= size/2 && c < size/2) {
-            count += (size * size / 4) * 2;
+        } else if(r >= size/2 && c < size/2) { // 3사분면
+            count += (size * size / 4) * 2; // 전체크기의 2/4만큼 더하고 시작
             find(size/2, r - size/2,c);
-        }else {
-            count += (size * size/4) * 3;
+        }else { // 4사분면
+            count += (size * size / 4) * 3; // 전체크기의 3/4만큼 더하고 시작
             find(size/2, r - size/2, c - size/2);
         }
     }
