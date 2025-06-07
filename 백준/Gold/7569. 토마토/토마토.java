f057e08b1224ccb345f392f8794bac55703fd32b
minseo003 <159864951+minseo003@users.noreply.github.com>
2025-06-07 16:46:56 +0900

    [Gold V] Title: 토마토, Time: 636 ms, Memory: 120840 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,94 +1,81 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+class point {
+    int x;
+    int y;
+    int h;
 
-public class Main {
-
-    static class Point {
-        int h;
-        int x;
-        int y;
-
-        Point(int h, int x, int y) {
-            this.h = h;
-            this.x = x;
-            this.y = y;
-        }
+    public point(int h,int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.h = h;
     }
+}
 
-    static int M;  //열
-    static int N;  //행
-    static int H;  //상자의 개수
+class Main {
+    static int n,m,h;
     static int[][][] board;
-    static int[] nx = {1,-1,0,0,0,0};
-    static int[] ny = {0,0,-1,1,0,0};
-    static int[] nh = {0,0,0,0,1,-1};
-    static Queue<Point> queue = new LinkedList<>();
+    static int[] dx = {0,1,0,-1,0,0};
+    static int[] dy = {1,0,-1,0,0,0};
+    static int[] dh = {0,0,0,0,-1,1};
     static boolean flag = false;
-    static int answer = Integer.MIN_VALUE;
+    static Queue<point> queue = new LinkedList<>();
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        m = Integer.parseInt(st.nextToken()); //가로길이
+        n = Integer.parseInt(st.nextToken()); //세로길이
+        h = Integer.parseInt(st.nextToken()); //높이
+        board = new int[h][n][m];
+
+        for(int i = 0 ; i < h ; i++) {
+            for(int j = 0; j < n ; j++) {
+                st = new StringTokenizer(br.readLine()," ");
+                for(int x = 0 ; x < m ; x++) {
+                    board[i][j][x] = Integer.parseInt(st.nextToken());
+                    if(board[i][j][x] == 1) queue.offer(new point(i,j,x));
+                    if(board[i][j][x] == 0) flag = true;
+                }
+            }
+        }
+        bfs();
+        int answer = Integer.MIN_VALUE;
+        for(int i = 0 ; i < h ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                for(int x = 0 ; x < m ; x++) {
+                    if(board[i][j][x] == 0) {
+                        System.out.println(-1);
+                        System.exit(0);
+                    }
+                    answer = Math.max(answer,board[i][j][x]);
+                }
+            }
+        }
+        System.out.println(answer-1);
+    }
 
     static void bfs() {
+        if(!flag) {
+            System.out.println(0);
+            System.exit(0);
+        }
 
         while(!queue.isEmpty()) {
+            point tmp = queue.poll();
 
-            Point point = queue.poll();
+            for(int i = 0 ; i < 6 ; i++) {
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
+                int nh = tmp.h + dh[i];
 
-            for (int i = 0; i < 6; i++) {
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && nh >= 0 && nh < h && board[nh][nx][ny] == 0) {
+                    queue.offer(new point(nh,nx,ny));
+                    board[nh][nx][ny] = board[tmp.h][tmp.x][tmp.y] + 1;
 
-                int dh = point.h + nh[i];
-                int dx = point.x + nx[i];
-                int dy = point.y + ny[i];
-
-                if (dh >= 0 && dh < H && dx >= 0 && dx < N && dy >= 0 && dy < M && board[dh][dx][dy] == 0) {
-                    board[dh][dx][dy] = board[point.h][point.x][point.y] + 1;
-                    queue.offer(new Point(dh,dx,dy));
                 }
             }
         }
-
-    }
-
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-        board = new int[H][N][M];
-        for (int k = 0; k < H; k++) {
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine()," ");
-                for (int j = 0; j < M; j++) {
-                    int tmp = Integer.parseInt(st.nextToken());
-                    board[k][i][j] = tmp;
-                    if (tmp == 1) queue.offer(new Point(k,i,j));
-                    if (tmp == 0) flag = true;
-                }
-            }
-        }
-
-        br.close();
-        bfs();
-
-        loop : for (int k = 0; k < H; k++) {
-                    for (int i = 0; i < N; i++) {
-                        for (int j = 0; j < M; j++) {
-                            if (board[k][i][j] == 0) {
-                                answer = -1;
-                                break loop;
-                            }
-                            answer = Math.max(answer,board[k][i][j]-1);
-                        }
-                    }
-                }
-
-        if (!flag) {
-            bw.write(0 + "\n");
-        } else {
-            bw.write(answer + "\n");
-        }
-        bw.flush();
-        bw.close();
     }
 }
